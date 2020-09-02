ec6f0261eff046200774a2137bb371d5694c53e3
RamKoushikReddy <xbhfuvx@gmail.com>
2020-09-02 17:20:15 +0530

    imported required modules and changes

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -7,9 +7,10 @@ import { HomeComponent } from './home/home.component';
 // Material Modules
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule, MatButtonModule],
+  imports: [CommonModule, HomeRoutingModule, MatButtonModule, MatListModule],
 })
 export class HomeModule {}
--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Podcast } from '../../shared/podcast';
+import { PodcastDirectoryService } from '../../shared/podcast-directory.service';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  allPodcasts: Observable<Podcast[]>;
 
-  constructor() { }
+  constructor(private pd: PodcastDirectoryService) {}
 
   ngOnInit(): void {
+    // this.pd.getPodcasts().subscribe((val) => {
+    //   this.allPodcasts = val;
+    // });
+    this.allPodcasts = this.pd.getPodcasts().pipe(tap(console.log));
   }
-
 }
