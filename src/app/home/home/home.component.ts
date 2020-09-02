ddce383f2a15f690165a69da24979cfc70326e0b
RamKoushikReddy <xbhfuvx@gmail.com>
2020-09-02 18:02:09 +0530

    added code to retrieve data

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Podcast } from '../../shared/podcast';
+
 import { PodcastDirectoryService } from '../../shared/podcast-directory.service';
 import { tap } from 'rxjs/operators';
 
@@ -10,14 +10,15 @@ import { tap } from 'rxjs/operators';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  allPodcasts: Observable<Podcast[]>;
+  allPodcasts: Observable<any[]>;
 
   constructor(private pd: PodcastDirectoryService) {}
 
   ngOnInit(): void {
-    // this.pd.getPodcasts().subscribe((val) => {
-    //   this.allPodcasts = val;
-    // });
     this.allPodcasts = this.pd.getPodcasts().pipe(tap(console.log));
   }
 }
+
+// this.pd.getPodcasts().subscribe((val) => {
+//   this.allPodcasts = val;
+// });
